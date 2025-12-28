57c7fd03054cae765de14e5860e5cc498d679ef5
sibyct <sibyct@gmail.com>
2025-12-28 23:45:12 +0100

    updated the Code for add a employee

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'time-tracker-header',
     standalone: true,
-    imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
+    imports: [CommonModule, MatToolbarModule,
+        MatButtonModule, MatIconModule,
+        MatFormFieldModule, MatInputModule
+    ],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
