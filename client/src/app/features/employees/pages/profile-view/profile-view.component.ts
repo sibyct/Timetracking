8feda88fb9a5d2e0bc46332bd033ec8b0a41b263
sibyct <sibyct@gmail.com>
2026-01-30 23:44:27 +0100

    reorganized the scss

--- client/src/app/features/employees/pages/profile-view/profile-view.component.ts
+++ client/src/app/features/employees/pages/profile-view/profile-view.component.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ProfileCardComponent } from '@features/employees/components/profile-card/profile-card.component';
 import { JobViewComponent } from '@features/employees/components/job-view/job-view.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'time-tracker-profile-view',
   standalone: true,
-  imports: [MatButtonModule, MatTabsModule, ProfileCardComponent, JobViewComponent],
+  imports: [MatButtonModule, MatTabsModule, ProfileCardComponent, JobViewComponent, MatToolbarModule],
   templateUrl: './profile-view.component.html',
   styleUrl: './profile-view.component.scss'
 })
--- client/src/app/layout/sidebar/sidebar.config.ts
+++ client/src/app/layout/sidebar/sidebar.config.ts
@@ -1,6 +1,6 @@
 import { APP_ROUTES } from '@shared/constants/routes';
 
 export const SIDEBAR_CONFIG = [
-    //{ label: 'Dashboard', icon: 'dashboard', route: APP_ROUTES.DASHBOARD },
+    { label: 'Dashboard', icon: 'dashboard', route: APP_ROUTES.DASHBOARD },
     { label: 'Employees', icon: 'groups', route: APP_ROUTES.EMPLOYEES },
 ]
